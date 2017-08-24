1988885d0ba39279c9ce3a8613c703bbdbc1bcd8
Yesung Cho <yesungc91@gmail.com>
2017-08-23 22:24:40 -0400

    Addition of MagicScroll Folder

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-alert("hello");
+// init ScrollMagic Controller
+var controller = new ScrollMagic.Controller();
+
+
+// Scale Scene
+var scale_scene = new ScrollMagic.Scene({
+  triggerElement: '#scale-trigger'
+})
+.setTween(scale_tween);
+
+
+controller.addScene([
+  scale_scene,
+]);
