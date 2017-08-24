40b063bff70b354a1c6a446a2f46f0c22ef3f4e4
Yesung Cho <yesungc91@gmail.com>
2017-08-23 23:09:54 -0400

    Bug 1.0
     TweenMax.to is not defined. Uncaught ReferenceError.

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
+// Scale Animation Setup
+// .to('@target', @length, {@object})
+// var scale_tween = TweenMax.to('#scale-animation', 1, {
+//   transform: 'scale(.75)',
+//   ease: Linear.easeNone
+// });
+
+// BG Animation Setup
+// .to('@target', @length, {@object})
+var bg_tween = TweenMax.to('#bg-trigger', 1, {
+  backgroundColor: '#FFA500',
+  ease: Linear.easeNone
+});
+
+// YoYo Animation Setup
+// .to(@target, @length, @object)
+var yoyo_tween = TweenMax.to('#yoyo-animation', 1, {
+  transform: 'scale(2)',
+  ease: Cubic.easeOut,
+  repeat: -1, // this negative value repeats the animation
+  yoyo: true // make it bounce…yo!
+});
+
+
 // init ScrollMagic Controller
 var controller = new ScrollMagic.Controller();
 
@@ -8,7 +32,20 @@ var scale_scene = new ScrollMagic.Scene({
 })
 .setTween(scale_tween);
 
+// Background Scene
+var bg_scene = new  ScrollMagic.Scene({
+  triggerElement: '#bg-trigger'
+})
+.setTween(bg_tween);
+
+// YoYo Scene
+var yoyo_scene = new  ScrollMagic.Scene({
+  triggerElement: '#yoyo-trigger'
+})
+.setTween(yoyo_tween);
 
 controller.addScene([
   scale_scene,
+  bg_scene,
+  yoyo_scene
 ]);
